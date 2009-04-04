0006f8fdf545c3d52f0f53eac309f538987138b3
katsuma <katsuma@76beb010-293a-44e9-8231-3d3008485057>
2009-04-04 06:23:08 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/botter@322 76beb010-293a-44e9-8231-3d3008485057

--- bin/twitter.rb
+++ bin/twitter.rb
@@ -19,7 +19,7 @@ cgi.out(
       Bot.tl.each do |screen_name, text|
         html += "<b><font color=red>" + screen_name + "</font></b><br>" + text.toutf8 + "<br><hr>"
       end
-      html.gsub!(/(http\S+)/) {"<font color=blue>#$1</font>" }
+      html.gsub!(/(http)/) { "<font color=blue>#$1</font>" }
       html.gsub!(/(@\w+)/) { "<font color=blue>#$1</font>" }
       CGI.unescapeHTML(html)
     end
