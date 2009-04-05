6af35ca080b3306150440d399db3324f440fe1ec
katsuma <katsuma@76beb010-293a-44e9-8231-3d3008485057>
2009-04-05 15:39:04 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/botter@359 76beb010-293a-44e9-8231-3d3008485057

--- bin/twitter.rb
+++ bin/twitter.rb
@@ -39,7 +39,7 @@ cgi.out(
     cgi.head{ cgi.title{'twitter'} } + cgi.body do
       debug = "uoo"
       if user and pass and comment
-        debug = Bot.update(user, pass, CGI.unescapeHTML(comment))
+        debug = Bot.update(user, pass, CGI.unescapeHTML(comment).toutf8)
       end
 
       html = $form + $link + user + pass + debug
