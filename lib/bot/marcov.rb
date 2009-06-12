c2319c6967996e1708b1e6ad064b2f58d9a17140
koko_master <koko_master@76beb010-293a-44e9-8231-3d3008485057>
2009-06-12 03:07:22 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/botter@428 76beb010-293a-44e9-8231-3d3008485057

---
+++ lib/bot/marcov.rb
@@ -0,0 +1,92 @@
+#!/usr/bin/ruby
+
+require 'MeCab'
+
+module Marcov
+  class Prefix
+    attr_reader :pref
+    MULTIPLIER = 31
+
+    def initialize(n, str)
+      @pref = []
+      n.times do
+        @pref << str
+      end
+    end
+
+    def hashcode
+      h = 0
+      @pref.each do |p|
+        h = MULTIPLIER * h + p.hashcode
+      end
+    end
+
+    def equals(p)
+      @pref.size.times do |i|
+        return false if !@pref[i].equals(p.pref[i])
+      end
+      return true
+    end
+  end
+
+  class Chain
+    NPREF = 2
+    NONWORD = "\n"
+
+    def initialize
+      @statetable = {}
+      @prefix = Prefix.new(NPREF, NONWORD)
+    end
+
+    def build(str)
+      begin
+        c = MeCab::Tagger.new(str)
+
+        p c.parse(str)
+
+        n = c.parseToNode(str)
+        while n do
+          print n.surface,  "\t", n.feature, "\t", n.cost, "\n"
+          n = n.next
+        end
+        print "EOS\n";
+      rescue
+        print "RuntimeError: ", $!, "\n";
+      end
+    end
+
+    def add(word)
+      suf = @statetab[prefix]
+      if !suf
+        suf = []
+        @statetable[Prefix.new(prefix)] = suf
+      end
+
+      suf << word
+      prefix.pref.shift
+      prefix.pref << word
+    end
+
+    def generate(nwords)
+      prefix = Prefix.new(NPREF, NONWORD)
+      nwords.times do
+        s = @statetable[prefix]
+        r = rand % s.size
+        suf = s[r]
+        break if suf == NONWORD
+        p suf
+        prefix.pref.shift
+        prefix.pref << suf
+      end
+    end
+  end
+
+  def self.markov(data, nwords=10000)
+    chain = Chain.new
+    chain.build(data)
+    #chain.generate(nwords)
+  end
+end
+
+#Marcov.markov(open($*[0]).read)
+Marcov.markov("すもももももももものうち")
