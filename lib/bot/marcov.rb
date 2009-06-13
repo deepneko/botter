8f4b83d825ff58efdb1b8ff46c9c2d870bd400fe
koko_master <koko_master@76beb010-293a-44e9-8231-3d3008485057>
2009-06-13 15:45:28 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/botter@441 76beb010-293a-44e9-8231-3d3008485057

--- lib/bot/marcov.rb
+++ lib/bot/marcov.rb
@@ -18,11 +18,11 @@ module Marcov
     def build(str)
       begin
         c = MeCab::Tagger.new(str)
-        p c.parse(str)
+        #p c.parse(str)
         n = c.parseToNode(str)
         while n do
           add(n.surface)
-          print n.surface,  "\t", n.feature, "\t", n.cost, "\n"
+          print n.surface, "\t", n.feature, "\t", n.cost, "\n"
           n = n.next
         end
       rescue
@@ -51,7 +51,7 @@ module Marcov
         r = rand(31) % s.size
         suf = s[r]
         break if suf == NONWORD
-        p CGI.unescapeHTML(suf)
+        #p CGI.unescapeHTML(suf)
         @prefix.shift
         @prefix << suf
       end
