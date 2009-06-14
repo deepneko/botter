dd67c3837071e2b1079c05df23a0cc3371287191
koko_master <koko_master@76beb010-293a-44e9-8231-3d3008485057>
2009-06-14 04:04:31 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/botter@444 76beb010-293a-44e9-8231-3d3008485057

---
+++ lib/bot/marcov.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+require 'MeCab'
+require 'cgi'
+$KCODE = 'u'
+
+module Marcov
+  NSIZE = 3
+
+  class Chain
+    def initialize
+      @words = []
+      @statetable = {}
+      @statetable.default = []
+    end
+
+    def build(str, n)
+      begin
+        mecab = MeCab::Tagger.new(str)
+        n = mecab.parseToNode(str)
+        while n do
+          @words << n
+          n = n.next
+        end
+
+        @words.size.times do |i|
+          p_word = n_word = @words[i]
+          j = i
+          while size(prev) > NSIZE
+            j = j + 1
+            n_word += @words[j] if j < @words.size
+          end
+          @statetable[p_word] << n_word
+        end
+      rescue
+        p "RuntimeError: ", $!;
+      end
+
+      p @statetable
+    end
+
+    def size(str)
+      str.split(//)
+    end
+
+    def generate(nwords)
+    end
+  end
+
+  def self.markov(data, nwords=10000)
+    chain = Chain.new
+    chain.build(data)
+    chain.generate(nwords)
+  end
+end
+
+Marcov.markov(open($*[0]).read, $*[1].to_i)
+
