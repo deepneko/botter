62a7606877b1bc3f58b744ea75fa9c7e177cc475
koko_master <koko_master@76beb010-293a-44e9-8231-3d3008485057>
2009-06-19 08:51:12 +0000

    git-svn-id: http://deepneko.dyndns.org/svn/repos/botter@518 76beb010-293a-44e9-8231-3d3008485057

--- lib/bot/botconst.rb
+++ lib/bot/botconst.rb
@@ -2,8 +2,6 @@ module Bot
   class BotConst
     attr_accessor :TWITTER_FRIENDS_TIMELINE
     attr_accessor :TWITTER_UPDATE
-    attr_accessor :DEFAULT_USER
-    attr_accessor :DEFAULT_PASS
     attr_accessor :PAGE_HISTORY
     attr_accessor :SLEEP_TIME
     attr_accessor :DB
@@ -14,8 +12,6 @@ module Bot
     def initialize
       @TWITTER_FRIENDS_TIMELINE = "http://twitter.com/statuses/friends_timeline.xml"
       @TWITTER_UPDATE = "http://twitter.com/statuses/update.xml"
-      @DEFAULT_USER = "deeptest"
-      @DEFAULT_PASS = "deepneko"
 
       # need configuration
       @PAGE_HISTORY = 5
